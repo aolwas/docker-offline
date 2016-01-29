2e654bcc71b667ec9326711a40feda6a03ee456f
realityone <realityone@me.com>
2016-01-29 21:18:43 +0800

    runable

--- centos/build.sh
+++ centos/build.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 if [[ -n $2 ]]; then
-    echo "generate centos by $1 at all."
-else
     echo "generate centos by $1 at $2"
+else
+    echo "generate centos by $1 at all."
 fi
 
 NAME="docker-build-centos-$1"
 
-if [[ docker inspect "$NAME" 2>&1 > /dev/null ]]; then
+if docker inspect "$NAME" 2>&1 > /dev/null; then
     docker rm -f "$NAME"
 fi
 
-docker run \
+docker run -it \
   --name="$NAME" \
+  --volume=`pwd`/data:/data \
+  --volume=`pwd`/build-inner:/usr/bin/build-inner \
   --entrypoint="/usr/bin/build-inner" \
   "centos:$1" $2
-
-docker cp build-inner "$NAME":/usr/bin/build-inner
-docker start -a $NAME
