a5f1c1d1109e982e517501fe8258ec8ad04c8d39
Kay Yan <kay.yan@daocloud.io>
2016-01-29 19:17:41 +0800

    update

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-echo "generate ubuntu by $1 at $2"
+echo "generate docker $3 in ubuntu $1 at $2"
 
 NAME="docker-build-ubuntu-$1"
 
 if docker inspect "$NAME" 2>&1 > /dev/null; then
+	docker kill "$NAME"
 	docker rm "$NAME"
 fi
 
 docker run \
   --name="$NAME" \
   --entrypoint="/usr/bin/build-inner" \
-  "ubuntu:$1"
+  "ubuntu:$1"  $3
 
 docker cp build-inner "$NAME":/usr/bin/build-inner
 
 docker start -a $NAME
+
+docker cp "$NAME":/var/cache/apt/archives $2
